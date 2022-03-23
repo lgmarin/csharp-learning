3439cf04faa72038fec954befe91e0f0f737a66e
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2022-03-23 18:42:16 -0300

    Consume API - Final

--- ConsumeApi/Controllers/WeatherForecastController.cs
+++ ConsumeApi/Controllers/WeatherForecastController.cs
@@ -19,22 +19,6 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public async Task<string> Get(string cityName)
     {
-        // var URL = $"?key=254970bdba1b4fc0b4c222531222203&q={cityName}&aqi=no";
-        
-        // // Reuse connections to the external API
-        // var httpClient = _httpClientFactory.CreateClient("weather");
-
-        // var response = await httpClient.GetAsync(URL);
-        
-        // return await response.Content.ReadAsStringAsync();
-
-
-        // using (var httpClient = new HttpClient())
-        // {
-        //     var response = await httpClient.GetAsync(URL);
-            
-        //     return await response.Content.ReadAsStringAsync();
-        // } SOLVES Multiple Socket problem for each request, but for different ?q it generates another socket MULTIPLE SOCKET PROBLEM
 
         return await _weatherService.Get(cityName);
 
